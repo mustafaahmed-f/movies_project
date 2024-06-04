1cb626da1b592827d6cc54325cdcd6eb9dd7cf1b
Mustafa Ahmed <mostafafikry97@gmail.com>
2024-06-04 17:55:15 +0300

    modifying details card

--- src/UI/MovieDetailsCard.tsx
+++ src/UI/MovieDetailsCard.tsx
@@ -16,8 +16,8 @@ const MovieDetailsCard: React.FC<myProps> = ({
   return (
     <div className="flex flex-col items-center gap-5 sm:items-start sm:w-1/2">
       <p className="mb-3 text-lg font-bold">{movie.title}</p>
-      <div className="flex flex-row gap-2 sm:items-center sm:gap-4 sm:flex-col md:flex-row">
-        <div className="flex items-center gap-3 ">
+      <div className="flex flex-row flex-wrap gap-3 sm:items-center sm:gap-4 sm:flex-col md:flex-row">
+        <div className="flex flex-wrap items-center gap-3">
           {movie.genres.map(
             (genre: { id: number; name: string }, i: number): JSX.Element => (
               <GenresContainer key={i} title={genre.name} />
